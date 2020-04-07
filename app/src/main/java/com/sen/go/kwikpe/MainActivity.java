a20503256d9308fafb05235eefd5b95e7e931663
POLOJURAJU <rajupoloju1@gmail.com>
2020-04-07 13:36:44 +0530

    new

---
+++ app/src/main/java/com/sen/go/kwikpe/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sen.go.kwikpe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.sen.go.workflow99.SharedPreferenceUtils;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button click;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        click = findViewById(R.id.click);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent in = new Intent(MainActivity.this, SharedPreferenceUtils.class);
+                startActivity(in);
+
+            }
+        });
+    }
+}
